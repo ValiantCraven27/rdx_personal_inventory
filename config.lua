fdc6c892bf6a0f253bf46176caf0645f73613b9e
Vision <77315942+ValiantCraven27@users.noreply.github.com>
2022-10-14 19:45:34 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 Config.IncludeCash = true -- Include cash in inventory?
-Config.IncludeWeapons = true-- Include weapons in inventory?
+Config.IncludeWeapons = false-- Include weapons in inventory?
 Config.IncludeAccounts = false -- Include accounts (bank, black money, ...)?
 Config.ExcludeAccountsList = {"bank"} -- List of accounts names to exclude from inventory
 Config.OpenControl = 0xC1989F95 -- Key for opening inventory. Edit html/js/config.js to change key for closing it.
